ab7237fa6fe2eebb2ac93d4e94d511b239f3df66
Abhishek R <abhiisheek@hotmail.com>
2024-04-23 22:18:27 +0530

    Updating file and checking diff

--- src/dir2/sample.java
+++ src/dir2/sample.java
@@ -2,5 +2,6 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     System.out.println("Hello!");
+	System.out.println("Updated files contents for demonstration of git diff");
   }
 }
