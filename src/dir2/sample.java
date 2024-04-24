5d5b0be993663ff911a2aaec62fa64e9ed9e1acb
Abhishek R <abhiisheek@hotmail.com>
2024-04-24 14:32:40 +0530

    Sample commit to trigger build

--- src/dir2/sample.java
+++ src/dir2/sample.java
@@ -3,5 +3,6 @@ public class App {
   public static void main(String[] args) throws Exception {
     System.out.println("Hello!");
 	System.out.println("Updated files contents for demonstration of git diff");
+	System.out.println("Commit to trigger build");
   }
 }
